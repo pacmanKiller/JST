54a02e431b631b392776332c51c925eeb6667244
pacmanKiller <philbalakirsky@gmail.com>
2015-06-04 09:28:17 -0400

    Update notesTestJavaTutorTesterAdd.java

--- notesTestJavaTutorTesterAdd.java
+++ notesTestJavaTutorTesterAdd.java
@@ -2,26 +2,56 @@
 public class notesTestJavaTutorTesterAdd extends JavaTutorTester {
 
 	public void addAllExercises() {
-		this.AddTutorExercise("JST", 1, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{1}}, {{5}}, {{3}}});
-		this.AddTutorExercise("JST", 1, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{2}}, {{6}}, {{4}}});
-		this.AddTutorExercise("JST", 1, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{5}}, {{9}}, {{7}}});
-		this.AddTutorExercise("JST", 1, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{1}}, {{5}}, {{3}}});
-		this.AddTutorExercise("JST", 1, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{2}}, {{10}}, {{6}}});
-		this.AddTutorExercise("JST", 1, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{5}}, {{25}}, {{15}}});
-		this.AddTutorExercise("JST", 1, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{6}}, {{3}}, {{1}}});
-		this.AddTutorExercise("JST", 1, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{12}}, {{6}}, {{2}}});
-		this.AddTutorExercise("JST", 1, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{24}}, {{12}}, {{4}}});
-		this.AddTutorExercise("JST", 1, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{6}}, {{-1}}, {{6}}});
-		this.AddTutorExercise("JST", 1, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{12}}, {{5}}, {{12}}});
-		this.AddTutorExercise("JST", 1, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{24}}, {{17}}, {{24}}});
-		this.AddTutorExercise("JST", 1, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{8}}, {{2}}, {{0}}});
-		this.AddTutorExercise("JST", 1, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{10}}, {{1}}, {{0}}});
-		this.AddTutorExercise("JST", 1, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{23}}, {{2}}, {{1}}});
+		this.AddTutorExercise("JST", 1, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{5}, {6}, {9}}, {{3}, {4}, {7}}});
+		this.AddTutorExercise("JST", 1, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{5}, {10}, {25}}, {{3}, {6}, {15}}});
+		this.AddTutorExercise("JST", 1, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{6}, {12}, {24}}, {{1}, {2}, {4}}, {{3}, {6}, {12}}});
+		this.AddTutorExercise("JST", 1, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{6}, {12}, {24}}, {{-1}, {5}, {17}}, {{6}, {12}, {24}}});
+		this.AddTutorExercise("JST", 1, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{8}, {10}, {23}}, {{2}, {1}, {2}}, {{0}, {0}, {1}}});
+		this.AddTutorExercise("JST", 1, 1, 6, "MethodCallReturnTestCase", new Object [][][] {{{8}, {10}, {24}}, {{2}, {4}, {0}}, {{4}, {5}, {12}}});
+		this.AddTutorExercise("JST", 1, 1, 7, "MethodCallReturnTestCase", new Object [][][] {{{3.456}, {5.675}, {21.345}}, {{3.0}, {5.0}, {21.0}}, {{3.456}, {5.675}, {21.345}}});
+		this.AddTutorExercise("JST", 1, 1, 8, "MethodCallReturnTestCase", new Object [][][] {{{1, 2, 3}, {2, 3, 4}, {3, 4, 5}}, {{-1}, {-2}, {-3}}, {{-1}, {2}, {7}}});
+		this.AddTutorExercise("JST", 1, 1, 9, "MethodCallReturnTestCase", new Object [][][] {{{1, 2, 3}, {5, 3, 4}, {11, 4, 5}}, {{1}, {2}, {3}}, {{1}, {5}, {11}}});
+		this.AddTutorExercise("JST", 1, 2, 1, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{9}, {10}, {13}}, {{5}, {6}, {9}}});
+		this.AddTutorExercise("JST", 1, 2, 2, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{2}, {4}, {10}}, {{-1}, {0}, {3}}});
+		this.AddTutorExercise("JST", 1, 2, 3, "MethodCallReturnTestCase", new Object [][][] {{{1, 2}, {5, 3}, {11, 4}}, {{3}, {4}, {5}}, {{2}, {6}, {12}}});
+		this.AddTutorExercise("JST", 1, 2, 4, "MethodCallReturnTestCase", new Object [][][] {{{1, 2}, {5, 3}, {11, 4}}, {{0}, {4}, {10}}, {{2}, {6}, {12}}});
+		this.AddTutorExercise("JST", 1, 3, 1, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{1*Math.sqrt(3)}, {2*Math.sqrt(3)}, {5*Math.sqrt(3)}}, {{1*Math.sqrt(2)}, {2*Math.sqrt(2)}, {5*Math.sqrt(2)}}});
+		this.AddTutorExercise("JST", 1, 3, 2, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {5}}, {{Math.sin(1)}, {Math.sin(2)}, {Math.sin(5)}}, {{Math.cos(1)}, {Math.cos(2)}, {Math.cos(5)}}});
+		this.AddTutorExercise("JST", 1, 3, 3, "MethodCallReturnTestCase", new Object [][][] {{{1, 2, 3}, {5, 3, 4}, {11, 4, 5}}, {{3.0}, {4.0}, {5.0}}, {{2.0}, {5.0}, {11.0}}});
+		this.AddTutorExercise("JST", 1, 3, 4, "MethodCallReturnTestCase", new Object [][][] {{{1}, {12}, {7}}, {{1.0}, {5.0}, {5.0}}, {{1.0}, {10.0}, {7.0}}});
+//		this.AddTutorExercise("JST", 1, 3, 5, "MethodCallRangeTestCase", new Object [][][] {{{}}, {{0, 6}}, {{1, 3}}});
+//		this.AddTutorExercise("JST", 1, 3, 6, "MethodCallRangeTestCase", new Object [][][] {{{}}, {{1,5}}, {{0, 4}}});
+		this.AddTutorExercise("JST", 2, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"rtl"}, {"ys "}, {"e l"}}, {{"rt"}, {"ys"}, {"e "}}});
+		this.AddTutorExercise("JST", 2, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"urtle "}, {"uys an"}, {"he lio"}}, {{"turtle "}, {"guys an"}, {"the lio"}}});
+		this.AddTutorExercise("JST", 2, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"urtl"}, {"uys "}, {"he l"}}, {{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}});
+		this.AddTutorExercise("JST", 2, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"rtle soup"}, {"ys and dolls"}, {"e lion king"}}, {{"tle soup"}, {"s and dolls"}, {" lion king"}}});
+		this.AddTutorExercise("JST", 2, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"urtle soup"}, {"uys and dolls"}, {"he lion king"}}, {{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}});
+		this.AddTutorExercise("JST", 2, 1, 6, "MethodCallReturnTestCase", new Object [][][] {{{}}, {{"XYZ"}}, {{"ABC"}}});
+		this.AddTutorExercise("JST", 2, 1, 7, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{12}, {15}, {14}}, {{11}, {14}, {13}}});
+		this.AddTutorExercise("JST", 2, 1, 8, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{22}, {28}, {26}}, {{6}, {6}, {6}}});
+		this.AddTutorExercise("JST", 2, 2, 1, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"turtle soupD"}, {"guys and dollsD"}, {"the lion kingD"}}, {{"turtle soupL"}, {"guys and dollsL"}, {"the lion kingL"}}});
+		this.AddTutorExercise("JST", 2, 2, 2, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}, {{"My turtle soup"}, {"My guys and dolls"}, {"My the lion king"}}, {{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}});
+		this.AddTutorExercise("JST", 2, 2, 3, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup", "trolls"}, {"guys and dolls", "potatoe"}, {"the lion king", "simba"}}, {{"turtle soup,trolls"}, {"guys and dolls,potatoe"}, {"the lion king,simba"}}, {{"turtle soup"}, {"guys and dolls"}, {"the lion king"}}});
+		this.AddTutorExercise("JST", 2, 2, 4, "MethodCallReturnTestCase", new Object [][][] {{{}}, {{""}}, {{"B"}}});
+		this.AddTutorExercise("JST", 2, 3, 1, "MethodCallReturnTestCase", new Object [][][] {{{"ABC"}, {"DEF"}, {""}}, {{false}, {true}, {false}}, {{true}, {false}, {false}}});
+		this.AddTutorExercise("JST", 2, 3, 2, "MethodCallReturnTestCase", new Object [][][] {{{"ABC"}, {"PQRST"}, {""}}, {{false}, {true}, {false}}, {{true}, {true}, {true}}});
+		this.AddTutorExercise("JST", 2, 3, 3, "MethodCallReturnTestCase", new Object [][][] {{{"turtle soup", "turtle soup"}, {"guys and dolls", "potatoe"}, {"the lion king", "simba"}}, {{true}, {false}, {false}}, {{true}, {true}, {true}}});
+		this.AddTutorExercise("JST", 2, 4, 1, "MethodCallReturnTestCase", new Object [][][] {{{"turGHItle sJKLoup"}, {"GHIuys and dolls"}, {"the lioJKLn king"}}, {{11}, {-1}, {7}}, {{3}, {0}, {-1}}});
+		this.AddTutorExercise("JST", 2, 4, 2, "MethodCallReturnTestCase", new Object [][][] {{{"MNO sJKLoup"}, {"GHIuys and MNOls"}, {"the lioJKLn king"}}, {{0}, {11}, {-1}}, {{-1}, {-1}, {-1}}});
+		this.AddTutorExercise("JST", 3, 1, 1, "MethodCallReturnTestCase", new Object [][][] {{{10}, {12}, {5}}, {{6}, {4}, {4}}, {{4}, {6}, {4}}});
+		this.AddTutorExercise("JST", 3, 1, 2, "MethodCallReturnTestCase", new Object [][][] {{{2}, {4}, {3}}, {{9}, {7}, {7}}, {{7}, {9}, {7}}});
+		this.AddTutorExercise("JST", 3, 1, 3, "MethodCallReturnTestCase", new Object [][][] {{{1}, {2}, {3}}, {{14}, {14}, {11}}, {{11}, {14}, {14}}});
+		this.AddTutorExercise("JST", 3, 1, 4, "MethodCallReturnTestCase", new Object [][][] {{{9}, {6}, {4}}, {{3}, {5}, {3}}, {{5}, {3}, {3}}});
+		this.AddTutorExercise("JST", 3, 1, 5, "MethodCallReturnTestCase", new Object [][][] {{{3}, {22}, {15}}, {{1}, {0}, {0}}, {{0}, {0}, {1}}});
+
+
+
 		
+
+
 	}
-	public static void main(String[] args) throws Exception{
+	public static void main(String[] args) throws Exception{		
 		JavaTutorTester testAdd = new notesTestJavaTutorTesterAdd();
-		System.out.println(testAdd.getExerciseList());
 		testAdd.runAllTests();
 //		System.out.print("notes' answer: \n" + testAdd.reportExerciseResults(testAdd.getExerciseList(), testAdd.runExercises(testAdd.getExerciseList())));
 	}
