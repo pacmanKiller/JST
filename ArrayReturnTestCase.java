ef4d0df7093bef865e450bf191ff4dcb49cbd937
pacmanKiller <philbalakirsky@gmail.com>
2015-06-10 09:38:57 -0400

    Update ArrayReturnTestCase.java

--- ArrayReturnTestCase.java
+++ ArrayReturnTestCase.java
@@ -9,32 +9,40 @@ public class ArrayReturnTestCase extends ExerciseTestCase {
 	public ArrayReturnTestCase(Object[][] description) {
 		super(description);
 	}
-
+	private String helpStringGenerator(String str){
+		String exceptionString = str;
+		String helpString = "";
+		
+		if (exceptionString.equals("ArrayIndexOutOfBoundsException") || exceptionString.equals("IndexOutOfBoundsException")){
+			helpString = "Check the size of the array and which element that is called.";
+		}
+		if (exceptionString.equals("NullPointerException")){
+			helpString = "Check if an array was created";
+		}
+		if (exceptionString.equals("IllegalArgumentException")){
+			helpString = "Check the parameters of the method being called";
+		}
+		return helpString;
+	}
 	@Override
 	public String runTest(Object[] parameters, Object[] expectedResult) {
 			Object [][] oldDes = this.getDescription();
 			Object [][] newDes = new Object [][] {parameters,expectedResult};
+			Object [] objectArray = new Object[]{};
 			this.setDescription(newDes);
-			Object fromCallMethod = null;
+			//Object fromCallMethod = null;
 			try{
-				fromCallMethod = this.callMethod();
+				objectArray = (Object []) this.callMethod();
+
 			} catch (Exception e){
 				StringWriter sw = new StringWriter();
 				e.printStackTrace(new PrintWriter(sw));
 				String exceptionString = sw.toString();
-				return "The test threw an exception while trying to call the method. " + exceptionString;
+				return "but invoking the method found caused an exception. The exception found was of type: " +
+				e.getClass().getSimpleName() + ". "
+				+ helpStringGenerator(e.getClass().getSimpleName());
 			}
 			this.setDescription(oldDes);
-			Object [] objectArray = new Object[]{};
-			try{
-				objectArray = (Object []) fromCallMethod;
-			} catch (Exception e){
-				StringWriter sw = new StringWriter();
-				e.printStackTrace(new PrintWriter(sw));
-				String exceptionString = sw.toString();
-
-				return "The test's return value was not an array! " + exceptionString;
-			}
 			
 				if (objectArray.length!=expectedResult.length){
 					return "The length of the passed array and the expected array is not equal!" + "\n" + 
