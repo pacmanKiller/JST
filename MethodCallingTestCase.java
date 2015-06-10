32367b342f901a3ceeddbb2ce93fd2d7a9c4b503
pacmanKiller <philbalakirsky@gmail.com>
2015-06-10 09:39:12 -0400

    Update MethodCallingTestCase.java

--- MethodCallingTestCase.java
+++ MethodCallingTestCase.java
@@ -5,7 +5,21 @@ public class MethodCallingTestCase extends ExerciseTestCase {
 	public MethodCallingTestCase(Object[][] description) {
 		super(description);
 	}
+	private String helpStringGenerator(String str){
+		String exceptionString = str;
+		String helpString = "";
 
+		if (exceptionString.equals("ArrayIndexOutOfBoundsException") || exceptionString.equals("IndexOutOfBoundsException")){
+			helpString = "Check the number of times Record was called.";
+		}
+		if (exceptionString.equals("NullPointerException")){
+			helpString = "Check if Record was called.";
+		}
+		if (exceptionString.equals("IllegalArgumentException")){
+			helpString = "Check the parameters of the method being called.";
+		}
+		return helpString;
+	}
 	public String runTest(Object[] parameters, Object[] expectedResult) {
 
 		Recorder.reset();
@@ -17,11 +31,13 @@ public class MethodCallingTestCase extends ExerciseTestCase {
 
 			callMethod();		
 		} catch (Exception e) {
-			//Fix this here, it should probably
-			System.out.println(e.getStackTrace());
+			e.printStackTrace();
+			
+			return "but invoking the method caused an exception. The exception found was of type: " +
+					e.getClass().getSimpleName() + ". "
+					+ helpStringGenerator(e.getClass().getSimpleName());
 		}
 		if(Recorder.getCalls().length==expectedResult.length){
-			
 			for (int a =0; a < Recorder.getCalls().length; a ++){
 				if(!(Recorder.getCalls()[a].equals(expectedResult[a]))){
 					if(badResultString==null){
@@ -54,14 +70,17 @@ public class MethodCallingTestCase extends ExerciseTestCase {
 
 	public String parameterDescription(Object[] parameters){
 		String called = "";
-		
-		for (int getCalls = 0; getCalls < Recorder.getCalls().length; getCalls ++ ){
+		if(parameters.length==0){
+			for (int getCalls = 0; getCalls < Recorder.getCalls().length; getCalls ++ ){
 
-			if(getCalls ==0){
-				called = "" + parameters [getCalls];
-			} else 
-				called += "," + parameters [getCalls];
+				if(getCalls ==0){
+					called = "" + parameters [getCalls];
+				} else 
+					called += "," + parameters [getCalls];
+			}
+			return "The record list was called with the following: " + called + " ";
+		}else{
+			return "The record list was called with no parameters, ";
 		}
-		return "The record list was called with the following: " + called + " ";
 	}
 }
